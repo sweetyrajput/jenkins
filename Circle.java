95f890efadff6734a7f84745029c148fa3f5a53e
Sweety <sweety.sweety@ibm.com>
2021-03-08 23:13:19 +0530

    pushed

---
+++ Circle.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		int r=8;
+		double area=2*3.14*r;
+		System.out.println("area of the circle is" +area);
+	}
+
+}
